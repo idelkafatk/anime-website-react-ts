7ee7253f163b89cea09a20f9f75b7bf6ef35f0a4
Idel Fatkhullin <maximbravin@gmail.com>
2023-08-20 17:58:00 +0600

    added craco build

--- src/services/anime/animeApiService.ts
+++ src/services/anime/animeApiService.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { apiToken } from '../../animeToken'
 
 const CORS_ANYWHERE_URL = 'http://localhost:8080/' // Адрес сервера CORS Anywhere
-const API_URL_MAL = 'api.myanimelist.net/v2'
+const API_URL_MAL = 'https://api.myanimelist.net/v2'
 const API_URL_UMAL = 'https://api.jikan.moe/v4' // unofficial mal
 
 const mal = axios.create({
